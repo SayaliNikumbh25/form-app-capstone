c2deed8c3a1f1e5ac84ac095c80d18e1c4169b41
SayaliNikumbh25 <sayalinikumbh7890@gmail.com>
2024-07-31 17:21:18 +0530

    initial commit

---
+++ server/app.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import env from 'dotenv';
+import mongoose from 'mongoose';
+import authRoutes from './routes/auth.js'
+import formRoutes from './routes/forms.js'
+import folderRoutes from './routes/folder.js'
+import cors from 'cors'
+import verifyToken from './middlewares/verifyToken.js';
+
+env.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.use('/api/forms', formRoutes);
+app.use('/api/folders',folderRoutes);
+
+app.get('/',(req,res)=>{
+    res.json({
+        Status: 'Sucess',
+        Message:'Api working fine'
+    })
+})
+
+app.use("*", (req, res) => {
+    res.status(404).json({
+        message: 'Endpoint not found',
+        status: 'Error',
+    });
+});
+
+
+app.listen(process.env.PORT, ()=>{
+    mongoose.connect(process.env.MONGODB_URL)
+    .then(()=>{console.log('Connected to database Successfuly')})
+    .then(()=> console.log(`Server is running at port ${process.env.PORT} `))
+   
+})
+
